3eb42b0ff87a2e517ff3a0956c82fcc47c60594d
bayubim <bayubimantara362@gmail.com>
2022-08-07 18:34:20 +0800

    navbar animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-var nav = document.querySelector('nav');
+var nav = document.querySelector("nav");
+window.addEventListener("scroll", function(){
+  nav.classList.toggle("scrolling",window.scrollY > 0);
+})
 
-      window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 5) {
-          nav.classList.add('bg-dark', 'shadow');
-        } else {
-          nav.classList.remove('bg-dark', 'shadow');
-        }
-      });
+// var nav = document.querySelector('nav');
+
+//       window.addEventListener('scroll', function () {
+//         if (window.pageYOffset > 5) {
+//           nav.classList.add('bg-dark', 'shadow');
+//         } else {
+//           nav.classList.remove('bg-dark', 'shadow');
+//         }
+//       });
