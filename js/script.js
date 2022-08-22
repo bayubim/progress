259e3a8ca95b3a6c5e12dbcab87f895796383e8f
BayuBim <bayubimantara362@gmail.com>
2022-08-23 01:19:21 +0800

    fix jumbotron

--- js/script.js
+++ js/script.js
@@ -3,12 +3,8 @@ window.addEventListener("scroll", function () {
   nav.classList.toggle("scrolling", window.scrollY > 0);
 });
 
-// var nav = document.querySelector('nav');
-
-//       window.addEventListener('scroll', function () {
-//         if (window.pageYOffset > 5) {
-//           nav.classList.add('bg-dark', 'shadow');
-//         } else {
-//           nav.classList.remove('bg-dark', 'shadow');
-//         }
-//       });
+let bg = document.getElementsByClassName("bg")[0];
+window.addEventListener("scroll", function () {
+  var value = window.scrollY;
+  bg.style.top = value * 0.5 + "px";
+});
